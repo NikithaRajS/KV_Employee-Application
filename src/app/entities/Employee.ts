5ce9d0c78e452650507bb09fcb5d23f74532076f
Nikitha <nikitharajhpk@gmail.com>
2022-08-01 20:40:54 +0530

    Modified Employee Entity

--- src/app/entities/Employee.ts
+++ src/app/entities/Employee.ts
@@ -8,6 +8,17 @@ import { Department } from "./Department";
         public id: string;
         @Column({ nullable: false })
         public name: string;
+        @Column({ nullable: false })
+        public joining_date: string;      //What format should be given to date?
+        @Column({ nullable: false })       //Any other constraints?
+        public role: string;  
+        @Column({ nullable: false })
+        public status: string; 
+        @Column({ nullable: false })
+        public experience: number;              
+        @Column({ nullable: false })
+        public address: number;    
+
         
         @ManyToOne(() => Department, { cascade: true })
         @JoinColumn()
