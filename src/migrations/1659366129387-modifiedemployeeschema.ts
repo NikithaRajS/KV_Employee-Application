494b44f7df29d2fab46cef2e44623b4ac10b5980
Nikitha <nikitharajhpk@gmail.com>
2022-08-01 20:34:43 +0530

    Modified Employee Entity

---
+++ src/migrations/1659366129387-modifiedemployeeschema.ts
@@ -0,0 +1,22 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class modifiedemployeeschema1659366129387 implements MigrationInterface {
+    name = 'modifiedemployeeschema1659366129387'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "employee" ADD "joining_date" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "employee" ADD "role" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "employee" ADD "status" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "employee" ADD "experience" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "employee" ADD "address" integer NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "address"`);
+        await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "experience"`);
+        await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "status"`);
+        await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "role"`);
+        await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "joining_date"`);
+    }
+
+}
