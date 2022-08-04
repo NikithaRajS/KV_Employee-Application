2dc5bc0159a6db3d5e8d2105f4e7085ab1080efe
Nikitha <nikitharajhpk@gmail.com>
2022-08-04 10:08:20 +0530

    Modified department error code

--- src/app/service/DepartmentService.ts
+++ src/app/service/DepartmentService.ts
@@ -5,59 +5,54 @@ import HttpException from "../exception/HttpException";
 import { DepartmentRepository } from "../repository/DepartmentRepository";
 import { ErrorCodes } from "../util/errorCode";
 
-export class DepartmentService{
-    constructor(private departmentRepo:DepartmentRepository){
+export class DepartmentService {
+  constructor(private departmentRepo: DepartmentRepository) {}
+  async getAllDepartments() {
+    return await this.departmentRepo.getAllDepartments();
+  }
+
+  async getDepartmentById(id: string) {
+    const department = await this.departmentRepo.getDepartmentById(id);
+    if (!department) {
+      throw new EntityNotFoundException(ErrorCodes.DEPARTMENT_NOT_FOUND);
     }
-    async getAllDepartments(){
-        return await this.departmentRepo.getAllDepartments()
+    return department;
+  }
+
+  public async createDepartment(departmentDetails: any) {
+    try {
+      const newDepartment = plainToClass(Department, {
+        name: departmentDetails.name,
+      });
+      const save = await this.departmentRepo.saveDepartmentDetails(
+        newDepartment
+      );
+      return save;
+    } catch (err) {
+      throw new HttpException(400, "Failed to create department", "Code-400");
     }
+  }
 
-    async getDepartmentById(id: string) {
-        const department= await this.departmentRepo.getDepartmentById(id);
-        if(!department){
-          throw new EntityNotFoundException(ErrorCodes.DEPARTMENT_NOT_FOUND)
-        }
-        return department;
-      }
-
-
-
-    public async createDepartment(departmentDetails: any) {
-        try {
-            const newDepartment = plainToClass(Department, {
-                name: departmentDetails.name,
-                
-            });
-            const save = await this.departmentRepo.saveDepartmentDetails(newDepartment);
-            return save;
-        } catch (err) {
-           // throw new HttpException(400, "Failed to create employee");
-        }
+  public async updateDepartmentById(id: string, departmentDetails: any) {
+    try {
+      const updatedDepartment = plainToClass(Department, {
+        name: departmentDetails.name,
+      });
+      const save = await this.departmentRepo.updateDepartmentDetails(
+        id,
+        updatedDepartment
+      );
+      return save;
+    } catch (err) {
+      throw new HttpException(400, "Failed to update department", "code-400");
     }
-   
-    public async updateDepartmentById(id: string, departmentDetails: any) {
-        try {
-          const updatedDepartment = plainToClass(Department, {
-            name: departmentDetails.name,
-            
-            
-          });
-          const save = await this.departmentRepo.updateDepartmentDetails(
-            id,
-            updatedDepartment
-          );
-          return save;
-        } catch (err) {
-          throw new HttpException(400, "Failed to create employee", "code-400");
-        }
-      }
+  }
 
-      async deleteDepartmentById (id: string) {
-        const department = await this.departmentRepo.getDepartmentById(id);
-        if(!department){
-          throw new EntityNotFoundException(ErrorCodes.EMPLOYEE_NOT_FOUND)
-        }
-        return await this.departmentRepo.softDeleteDepartmentById(id);
-      }
-    
+  async deleteDepartmentById(id: string) {
+    const department = await this.departmentRepo.getDepartmentById(id);
+    if (!department) {
+      throw new EntityNotFoundException(ErrorCodes.DEPARTMENT_NOT_FOUND);
     }
+    return await this.departmentRepo.softDeleteDepartmentById(id);
+  }
+}
